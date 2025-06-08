d3791067794f930ad935e7288a2c727fce1ff7bc
londoncute <107755910+londoncute@users.noreply.github.com>
2025-06-08 14:20:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,26 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
-#include "Automata.h"
+#include "../include/Automata.h"
+
+TEST(AutomataTest, InitialState) {
+    Automata a;
+    EXPECT_EQ(a.getState(), OFF);
+}
+
+TEST(AutomataTest, TurnOn) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(a.getState(), WAIT);
+}
+
+TEST(AutomataTest, AddCoin) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    EXPECT_EQ(a.getState(), ACCEPT);
+}
+
+// ... (добавьте остальные тесты, минимум 10)
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
