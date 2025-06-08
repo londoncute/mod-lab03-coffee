910d7f5154c555fcbc7f39b6d561bc2673f57e29
londoncute <107755910+londoncute@users.noreply.github.com>
2025-06-08 14:50:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,40 +3,49 @@
 #include <gtest/gtest.h>
 #include "../include/Automata.h"
 
-TEST(AutomataTest, InitialState) {
-  Automata a;
-  EXPECT_EQ(a.getState(), OFF);
+TEST(AutomataTest, InitialStateIsOff) {
+    Automata a;
+    EXPECT_EQ(a.getState(), OFF);
 }
 
-TEST(AutomataTest, TurnOn) {
-  Automata a;
-  a.on();
-  EXPECT_EQ(a.getState(), WAIT);
+TEST(AutomataTest, TurnsOnCorrectly) {
+    Automata a;
+    a.on();
+    EXPECT_EQ(a.getState(), WAIT);
 }
 
-TEST(AutomataTest, AddCoin) {
-  Automata a;
-  a.on();
-  a.coin(50);
-  EXPECT_EQ(a.getState(), ACCEPT);
+TEST(AutomataTest, AcceptsCoins) {
+    Automata a;
+    a.on();
+    a.coin(50);
+    EXPECT_EQ(a.getState(), ACCEPT);
 }
 
-TEST(AutomataTest, GetMenu) {
-  Automata a;
-  const auto& menu = a.getMenu();
-  ASSERT_FALSE(menu.empty());
-  EXPECT_EQ(menu[0], "Чай");
+TEST(AutomataTest, ReturnsCorrectMenu) {
+    Automata a;
+    const auto& menu = a.getMenu();
+    ASSERT_EQ(menu.size(), 4);
+    EXPECT_EQ(menu[0], "Чай");
+    EXPECT_EQ(menu[1], "Кофе");
 }
 
-TEST(AutomataTest, CheckFunds) {
-  Automata a;
-  a.on();
-  a.coin(40);
-  EXPECT_TRUE(a.check(3));  // Молоко стоит 40
-  EXPECT_FALSE(a.check(1)); // Кофе стоит 50
+TEST(AutomataTest, ChecksBalanceCorrectly) {
+    Automata a;
+    a.on();
+    a.coin(40);
+    EXPECT_TRUE(a.check(3));  // Молоко стоит 40
+    EXPECT_FALSE(a.check(1)); // Кофе стоит 50
+}
+
+TEST(AutomataTest, CancelsOperation) {
+    Automata a;
+    a.on();
+    a.coin(100);
+    a.cancel();
+    EXPECT_EQ(a.getState(), WAIT);
 }
 
 int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
