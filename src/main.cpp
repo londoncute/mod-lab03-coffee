f59cf0053413bfbf101bcb1bcfacd5569e845f3b
londoncute <107755910+londoncute@users.noreply.github.com>
2025-06-08 14:28:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
+// Copyright 2025 Olenev Dmitri
 #include <iostream>
 #include "../include/Automata.h"
 
 int main() {
-    Automata coffeeMachine;
+  Automata coffeeMachine;
 
-    coffeeMachine.on();
-    coffeeMachine.coin(100);
-    
-    std::cout << "Меню:" << std::endl;
-    auto menu = coffeeMachine.getMenu();
-    for (size_t i = 0; i < menu.size(); i++) {
-        std::cout << i << ". " << menu[i] << std::endl;
-    }
+  coffeeMachine.on();
+  coffeeMachine.coin(100);
+  
+  std::cout << "Меню:" << std::endl;
+  auto menu = coffeeMachine.getMenu();
+  for (size_t i = 0; i < menu.size(); i++) {
+    std::cout << i << ". " << menu[i] << std::endl;
+  }
 
-    coffeeMachine.choice(1);  // Выбираем кофе (цена 50 руб.)
-    coffeeMachine.off();
+  coffeeMachine.choice(1);
+  coffeeMachine.off();
 
-    return 0;
+  return 0;
 }
