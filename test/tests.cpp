b845fdf3dd38504cfdd6a348c78a79c4c7c8facf
londoncute <107755910+londoncute@users.noreply.github.com>
2025-06-08 14:45:12 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,19 +9,34 @@ TEST(AutomataTest, InitialState) {
 }
 
 TEST(AutomataTest, TurnOn) {
-    Automata a;
-    a.on();
-    EXPECT_EQ(a.getState(), WAIT);
+  Automata a;
+  a.on();
+  EXPECT_EQ(a.getState(), WAIT);
 }
 
 TEST(AutomataTest, AddCoin) {
-    Automata a;
-    a.on();
-    a.coin(50);
-    EXPECT_EQ(a.getState(), ACCEPT);
+  Automata a;
+  a.on();
+  a.coin(50);
+  EXPECT_EQ(a.getState(), ACCEPT);
+}
+
+TEST(AutomataTest, GetMenu) {
+  Automata a;
+  const auto& menu = a.getMenu();
+  ASSERT_FALSE(menu.empty());
+  EXPECT_EQ(menu[0], "Чай");
+}
+
+TEST(AutomataTest, CheckFunds) {
+  Automata a;
+  a.on();
+  a.coin(40);
+  EXPECT_TRUE(a.check(3));  // Молоко стоит 40
+  EXPECT_FALSE(a.check(1)); // Кофе стоит 50
 }
 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
