7795a62f70063f2756ca2ccb5a747f1bd15ec857
londoncute <107755910+londoncute@users.noreply.github.com>
2025-06-08 14:35:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,11 @@ int main() {
 
   coffeeMachine.on();
   coffeeMachine.coin(100);
-  
   std::cout << "Меню:" << std::endl;
   auto menu = coffeeMachine.getMenu();
   for (size_t i = 0; i < menu.size(); i++) {
     std::cout << i << ". " << menu[i] << std::endl;
   }
-
   coffeeMachine.choice(1);
   coffeeMachine.off();
 
